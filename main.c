625739717c52d316db06d7b36c5fd8ccb512c999
HengBun <hengkybuntoro@gmail.com>
2018-07-04 16:25:22 +0700

    Remap to ProMicro. Replace to ProMicro Pinout in docs folder. Added macro to change directional output mode.

--- main.c
+++ main.c
@@ -44,9 +44,14 @@ void setup_pins(void);
 
 int main(void) {
 	uint8_t pad_up, pad_down, pad_left, pad_right, pad_y, pad_b, pad_x, pad_a, pad_black,
-	pad_white, pad_start, pad_select, pad_l3, pad_r3, pad_l, pad_r, pad_left_analog_x,
+	pad_white, pad_start, pad_select, pad_home, pad_l3, pad_r3, pad_l, pad_r, pad_left_analog_x,
 	pad_left_analog_y, pad_right_analog_x, pad_right_analog_y;
-
+	
+	bool mode_DPAD, mode_Lanalog, mode_Ranalog;
+	mode_DPAD=true;
+	mode_Lanalog=false;
+	mode_Ranalog=false;
+	
 	// Set clock @ 16Mhz
 	CPU_PRESCALE(0);
 
@@ -63,57 +68,65 @@ int main(void) {
 	// Pins polling and gamepad status updates
 	for (;;) {
 		xbox_reset_watchdog();
-
-		pad_up = !bit_check(PINC, 7);
-		pad_down = !bit_check(PINB, 2);
-		pad_left = !bit_check(PINB, 0);
-		pad_right = !bit_check(PIND, 3);
-		pad_y = !bit_check(PIND, 2);
-		pad_b = !bit_check(PIND, 1);
-		pad_x = !bit_check(PIND, 0);
-		pad_a = !bit_check(PIND, 4);
-		pad_black =  !bit_check(PINC, 6);
-		pad_white =  !bit_check(PIND, 7);
-		pad_start =  !bit_check(PINE, 6);
-		pad_select =  !bit_check(PINB, 4);
-		pad_l3 =  !bit_check(PINB, 5);
-		pad_r3 =  !bit_check(PINB, 6);
-		pad_l = !bit_check(PINB, 7);
-		pad_r = !bit_check(PIND, 6);
+		
+		pad_up = !bit_check(PIND, 3);
+		pad_down = !bit_check(PIND, 2);
+		pad_left = !bit_check(PIND, 1);
+		pad_right = !bit_check(PIND, 0);
+		pad_x = !bit_check(PIND, 4); //square
+		pad_y = !bit_check(PINC, 6); //triangle 
+		pad_a = !bit_check(PIND, 7); //x 
+		pad_b = !bit_check(PINE, 6); //circle
+		pad_black =  !bit_check(PINB, 4); //lb
+		pad_white =  !bit_check(PINB, 5); //rb
+		pad_l = !bit_check(PINB, 6); //lt 
+		pad_r = !bit_check(PINB, 2); //rt
+		pad_start =  !bit_check(PINB, 3);
+		pad_select =  !bit_check(PINB, 1);
+		pad_home = !bit_check(PINF, 7);
+		pad_l3 =  !bit_check(PINF, 6);
+		pad_r3 =  !bit_check(PINF, 5);
 
 		pad_left_analog_x = pad_left_analog_y = pad_right_analog_x = pad_right_analog_y = 0x7F;
 
-		if(!bit_check(PINB, 1)) {
-			pad_left_analog_x = 0x00;
-		} else if(!bit_check(PINB, 3)) {
-			pad_left_analog_x = 0xFF;
+	if(mode_Lanalog){
+		if(!bit_check(PIND, 1)) {
+			pad_left_analog_x = 0x00; //left Analog left
+		} else if(!bit_check(PIND, 0)) {
+			pad_left_analog_x = 0xFF; //left Analog right
 		}
 
-		if(!bit_check(PINF, 0)) {
-			pad_left_analog_y = 0x00;
-		} else if(!bit_check(PINF, 1)) {
-			pad_left_analog_y = 0xFF;
+		if(!bit_check(PIND, 3)) {
+			pad_left_analog_y = 0x00; //left Analog Up
+		} else if(!bit_check(PIND, 2)) {
+			pad_left_analog_y = 0xFF; //left Analog Down
 		}
-
-		if(!bit_check(PINF, 4)) {
+	}
+	
+	if(mode_Ranalog){
+		if(!bit_check(PIND, 1)) {
 			pad_right_analog_x = 0x00;
-		} else if(!bit_check(PINF, 5)) {
+		} else if(!bit_check(PIND, 0)) {
 			pad_right_analog_x = 0xFF;
 		}
 
-		if(!bit_check(PINF, 6)) {
+		if(!bit_check(PIND, 3)) {
 			pad_right_analog_y = 0x00;
-		} else if(!bit_check(PINF, 7)) {
+		} else if(!bit_check(PIND, 2)) {
 			pad_right_analog_y = 0xFF;
 		}
-
+	}
+	
+	if(mode_DPAD){
 		pad_up    ? bit_set(gamepad_state.digital_buttons_1, XBOX_DPAD_UP)    : bit_clear(gamepad_state.digital_buttons_1, XBOX_DPAD_UP);
 		pad_down  ? bit_set(gamepad_state.digital_buttons_1, XBOX_DPAD_DOWN)  : bit_clear(gamepad_state.digital_buttons_1, XBOX_DPAD_DOWN);
 		pad_left  ? bit_set(gamepad_state.digital_buttons_1, XBOX_DPAD_LEFT)  : bit_clear(gamepad_state.digital_buttons_1, XBOX_DPAD_LEFT);
 		pad_right ? bit_set(gamepad_state.digital_buttons_1, XBOX_DPAD_RIGHT) : bit_clear(gamepad_state.digital_buttons_1, XBOX_DPAD_RIGHT);
-
+	}
+	
 		pad_start  ? bit_set(gamepad_state.digital_buttons_1, XBOX_START)       : bit_clear(gamepad_state.digital_buttons_1, XBOX_START);
 		pad_select ? bit_set(gamepad_state.digital_buttons_1, XBOX_BACK)        : bit_clear(gamepad_state.digital_buttons_1, XBOX_BACK);
+		pad_home   ? bit_set(gamepad_state.digital_buttons_2, XBOX_HOME)		: bit_clear(gamepad_state.digital_buttons_2, XBOX_HOME);
 		pad_l3     ? bit_set(gamepad_state.digital_buttons_1, XBOX_LEFT_STICK)  : bit_clear(gamepad_state.digital_buttons_1, XBOX_LEFT_STICK);
 		pad_r3     ? bit_set(gamepad_state.digital_buttons_1, XBOX_RIGHT_STICK) : bit_clear(gamepad_state.digital_buttons_1, XBOX_RIGHT_STICK);
 
@@ -125,10 +138,22 @@ int main(void) {
 		pad_black ? bit_set(gamepad_state.digital_buttons_2, XBOX_LB)    : bit_clear(gamepad_state.digital_buttons_2, XBOX_LB);
 		pad_white ? bit_set(gamepad_state.digital_buttons_2, XBOX_RB)    : bit_clear(gamepad_state.digital_buttons_2, XBOX_RB);
 
-		if(pad_start && pad_select) {
-			bit_set(gamepad_state.digital_buttons_2, XBOX_HOME);
-		} else {
-			bit_clear(gamepad_state.digital_buttons_2, XBOX_HOME);
+		if(pad_start && pad_x) {
+			mode_DPAD=true;
+			mode_Lanalog=false;
+			mode_Ranalog=false;
+		}
+		
+		if(pad_start && pad_y)	{
+			mode_DPAD=false;
+			mode_Lanalog=true;
+			mode_Ranalog=false;
+		}
+		
+		if(pad_start && pad_a)	{
+			mode_DPAD=false;
+			mode_Lanalog=false;
+			mode_Ranalog=true;
 		}
 
 		gamepad_state.l_x = pad_left_analog_x * 257 + -32768;
